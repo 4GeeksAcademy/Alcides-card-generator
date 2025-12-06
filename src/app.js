1d15567c120a2c00c3c12707d20b41887073a2cc
Alcides Rivera <elarsi626@gmail.com>
2025-12-06 17:05:06 +0000

    Segundo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+
+const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const palos = ["♦", "♠", "♥ ", "♣"];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function newCard() {
+  let getNumeros = Math.floor(Math.random() * numeros.length);
+  let getPalos = Math.floor(Math.random() * palos.length);
+  return { "numero": numeros[getNumeros], "palo": palos[getPalos] }
+}
+
+function superFuncion() {
+let randomCard = newCard()
+document.getElementById("arriba").textContent = `${randomCard.palo}`
+document.getElementById("medio").textContent = `${randomCard.numero}`
+document.getElementById("abajo").textContent = `${randomCard.palo}`
+}
+
+document.getElementById("boton").addEventListener("click", superFuncion); 
+
+setInterval(superFuncion, 10000);
+ 
+
